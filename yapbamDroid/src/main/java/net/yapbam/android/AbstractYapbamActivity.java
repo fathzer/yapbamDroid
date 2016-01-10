e9f7d3faed344970d3d09f47a59e6f12a51a119a
fathzer <jm-dev@astesana.net>
2016-01-10 19:19:21 +0100

    Removes unused dependencies

--- yapbamDroid/src/main/java/net/yapbam/android/AbstractYapbamActivity.java
+++ yapbamDroid/src/main/java/net/yapbam/android/AbstractYapbamActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.text.Html;
 import android.text.InputType;
 import android.view.View;
